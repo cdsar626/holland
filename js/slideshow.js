ea91b1bebe1a671fc8b93e2d945dddd14242e7f1
César De la Vega <cdsar626@gmail.com>
2019-02-21 02:21:29 -0400

    Melvin view added.

---
+++ js/slideshow.js
@@ -0,0 +1,66 @@
+//slideshow made by @cdsar626
+
+let slideshow_picture_active = 0;
+let picUrls = {};
+
+function slideshow(target, urls) {
+  picUrls[target] = urls;
+  let div = document.getElementsByClassName(target);
+  console.log(target);
+  if (div.length) {
+    div = div[0];
+    div.className = div.className + ' slideshow-box';
+    console.log(div);
+    let finalHtml = `
+    <div class="slideshow-picture">
+      <div class="slideshow-left" onclick="prevbg('${target}')"></div>
+      <img class="mainPicture${target}" src="${urls[0]}">
+      <div class="slideshow-right" onclick="nextbg('${target}')"></div>
+    </div>`;
+    finalHtml += `<div class="slideshow-thumbs">`;
+    for (let i = 0; i < urls.length; i++) {
+      finalHtml += `<div class="slideshow-thumbs-picture ${i == 0 ? 'slideshow-active':''}" style="
+      background-image: url(${urls[i]});
+      background-position: center;"
+      onclick="changebg(this, '${target}', ${i})"></div>`;
+    }
+    finalHtml += '</div>';
+    div.innerHTML = finalHtml;
+  }
+}
+
+function changebg(ev, trgt, active) {
+  console.log(ev);
+  let old = document.getElementsByClassName('slideshow-active')[0];
+  let oldClasses = old.className.split(' slideshow-active');
+  old.className = oldClasses[0]+oldClasses[1];
+  ev.className += ' slideshow-active';
+  let pic = document.getElementsByClassName('mainPicture'+trgt)[0];
+  console.log(pic);
+  pic.src = ev.style.backgroundImage.split('"')[1];
+  slideshow_picture_active = active;
+}
+
+function nextbg(trgt) {
+  let old = document.getElementsByClassName('slideshow-active')[0];
+  let oldClasses = old.className.split(' slideshow-active');
+  old.className = oldClasses[0]+oldClasses[1];
+  let pic = document.getElementsByClassName('mainPicture'+trgt)[0];
+  console.log(picUrls[trgt]);
+  slideshow_picture_active = (slideshow_picture_active + 1)% picUrls[trgt].length;
+  pic.src = picUrls[trgt][slideshow_picture_active];
+  let next = document.getElementsByClassName('slideshow-thumbs-picture')[slideshow_picture_active];
+  next.className += ' slideshow-active';
+}
+
+function prevbg(trgt) {
+  let old = document.getElementsByClassName('slideshow-active')[0];
+  let oldClasses = old.className.split(' slideshow-active');
+  old.className = oldClasses[0]+oldClasses[1];
+  let pic = document.getElementsByClassName('mainPicture'+trgt)[0];
+  console.log(picUrls[trgt]);
+  slideshow_picture_active = (slideshow_picture_active == 0 ? picUrls[trgt].length : slideshow_picture_active) - 1;
+  pic.src = picUrls[trgt][slideshow_picture_active];
+  let next = document.getElementsByClassName('slideshow-thumbs-picture')[slideshow_picture_active];
+  next.className += ' slideshow-active';
+}
